8e4d874397cd31ecc6b26254ddc7a9ab28555e3f
XunM <hao@xmint.cn>
2020-08-20 21:59:42 +0800

    主页渲染

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,21 +1,122 @@
 <template>
-    <div>
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img class="logo" src="../assets/images/logo.png" alt />
+        <span>电商后台管理系统</span>
         <el-button type="info" @click="logout">退出登录</el-button>
-    </div>
+      </div>
+    </el-header>
+    <!-- 身体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <div class="toggle_button">
+          <i class="el-icon-s-fold"></i>
+        </div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+        >
+          <el-submenu :index="item.id+''" v-for="(item) in menulsit" :key="item.id">
+            <template slot="title">
+              <i :class="menuIcon[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+
+            <el-menu-item
+              :index="subitem.id+''"
+              v-for="(subitem) in item.children"
+              :key="subitem.id"
+            >
+              <i class="el-icon-menu"></i>
+              <span>{{subitem.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    methods: {
-        logout(){
-            window.sessionStorage.clear();
-            this.$router.push('/login');
-            this.$Message("你已退出登录");
-        }
+  created() {
+    this.getMenuList();
+  },
+  data() {
+    return {
+      menulsit: [],
+      menuIcon: {
+        "125": "el-icon-user-solid",
+        "103": "el-icon-s-check",
+        "101": "el-icon-s-goods",
+        "102": "el-icon-s-order",
+        "145": "el-icon-s-data",
+      },
+    };
+  },
+  methods: {
+    //退出登录
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+      this.$Message("你已退出登录");
     },
-}
+    //获取菜单数据
+    async getMenuList() {
+      let { data: res } = await this.$http.get("menus");
+      if (res.meta.status != 200) return this.$Message.error("数据获取失败");
+      this.menulsit = res.data;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
 
+.el-header {
+  background-color: #373b41;
+  .logo {
+    margin-top: 5px;
+    width: 50px;
+    margin-right: 15px;
+  }
+  span {
+    font-size: 20px;
+    color: #fff;
+    vertical-align: top;
+    line-height: 60px;
+  }
+  .el-button {
+    margin-top: 10px;
+    float: right;
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+  .toggle_button {
+    font-size: 26px;
+    background-color: #4a5064;
+    color: #fff;
+    text-align: center;
+    cursor: pointer;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import './assets/css/global.css'
 import axios from 'axios'
 
 //设置axios默认请求路径
-axios.defaults.baseURL ='http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//请求拦截器
+axios.interceptors.request.use(config => {
+  //为请求头添加token字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
